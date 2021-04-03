f223f9c9e145760b8b22f608696d613bf995bc66
sumeyyeozel <e170503022@stud.tau.edu.tr>
2021-04-03 16:44:57 +0300

    checkin'e kadar olan sayfalar yapıldı.

--- Software_Projekt/src/software_projekt/FXMLDocumentController.java
+++ Software_Projekt/src/software_projekt/FXMLDocumentController.java
@@ -7,24 +7,77 @@ package software_projekt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author sumey
  */
 public class FXMLDocumentController implements Initializable {
+    //Hauptmenu choicebox list
+    ObservableList<String> h_choicelist= FXCollections.
+            observableArrayList("Testbetrieb", "Handbetrieb");
     
+     //Anmeldung
     @FXML
-    private Label label;
+    private Button anmelden;
+    @FXML 
+    private TextField benutzererk;
+    @FXML 
+    private PasswordField pass;
+     //Hauptmenu 
+    @FXML
+    private Button h_weiter;
+    @FXML
+    private Button h_ende;
+    @FXML
+    private ChoiceBox h_choice;
+    //SETUP
+    @FXML
+    private Button set_weiter;
+    @FXML
+    private Button set_abbrechen;
+    @FXML
+    private TextField set_tf_artikel;
+    @FXML
+    private TextField set_tf_auftrag;
+    //initialisierung
+    @FXML
+    private Button init_beginn;
+    @FXML
+    private Button init_abbrechen;
+    //initialisierung ende
+    @FXML
+    private Button iende_checkin;
+    @FXML
+    private Button iende_abbrechen;
+    //initialisierun ende alert
+    @FXML
+    private Button ialert_abbrechen;
+    
+    //hauptmenu choicebox aktifleştirme ama oldu mu bilmiyorum.
+    @FXML
+    private void initialize(){
+      h_choice.setValue("Testbetrieb");
+      h_choice.setItems(h_choicelist);
+      
+
+
+}
+    
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        
     }
     
     @Override
