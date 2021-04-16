f9d73b39bbf624a1f482d5b7d4830fb9fd12f8e1
sumeyyeozel <e170503022@stud.tau.edu.tr>
2021-04-16 12:25:14 +0300

    controllerlar eklendi, tek bi klasörde, cabinet mock ve message type ayrı bi klasöre taşındı.

--- Software_Projekt/src/controller/FXMLDocumentController.java
+++ Software_Projekt/src/controller/FXMLDocumentController.java
@@ -11,7 +11,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
