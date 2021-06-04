be85b7779ae30ae7d5950786bda3bd8ba462a700
nurdanrseker <nurdanrseker@gmail.com>
2021-06-04 21:12:09 +0300

    Check_in1 ara yuzune gecis yapildi.

--- Software_Projekt/src/software_projekt/controller/InitialisierungController.java
+++ Software_Projekt/src/software_projekt/controller/InitialisierungController.java
@@ -9,10 +9,15 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import software_projekt.Database.DB_Connection;
 import software_projekt.Software_Projekt;
 import static software_projekt.Software_Projekt.connect2Cabinet;
@@ -35,28 +40,78 @@ public class InitialisierungController implements Initializable {
     @FXML
     private Button init_beginn;
     @FXML
-    private Button init_abbrechen;
-    
+    private Button init_weiter;
+    @FXML
+    private Label begincheck;
+    String answer;
 
     @FXML
     private void InitBegin(MouseEvent event) throws IOException {
                 
                 
-		String message = "STRT|Kabinett 1|Nurdanseker|TESTER|10|3";
+		String message = "STRT|Kabinett 1|Nurdanseker|ADMIN|10|3";
 		System.out.println("===>> " + message);
                 //Software_Projekt sp= new Software_Projekt();
                //Software_Projekt.connect2Cabinet(message);
 		Software_Projekt.toCabinet.println(message);
-               System.out.println(Software_Projekt.fromCabinet.readLine());
-             
+                String answer=Software_Projekt.fromCabinet.readLine();
+                System.out.println(answer);
+                setLabel();
+                 
+        
                 
     }
 
+    
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO 
         
-     
-        
+           
+
+    } 
+    private void setLblError(Color color, String text) {
+        begincheck.setTextFill(color);
+        begincheck.setText(text);
+        System.out.println(text);
     }
     
+    public void setLabel(){
+    if (answer!=null&&answer.startsWith("Cabinet")) {
+                 begincheck.setTextFill(Color.GREEN);
+                     begincheck.setText("Cabinet connection : Successful");
+                     } else {
+                begincheck.setTextFill(Color.TOMATO);
+                begincheck.setText("Cabinet could not be connected: Fail ");}
+    }
+
+    @FXML
+    private void Init_Weiter(MouseEvent event) throws IOException {
+        
+        
+        if (event.getSource() == init_weiter) {
+            //login here
+            //if (init_weiter().equals("Success")) {
+              //  try {
+
+                    //add you loading or delays - ;-)
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    //stage.setMaximized(true);
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/software_projekt/fxml/Check_in1.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+
+               // } catch (IOException ex) {
+                 //   System.err.println(ex.getMessage());
+                //}
+
+            //}
+        }
+        
+        
+        
+        
+    }
 }
